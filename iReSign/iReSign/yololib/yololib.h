c9ac74545768b302b5fd509a40905377e325ab7c
Joey <qiaoxueshi@gmail.com>
2016-03-23 01:43:16 +0800

    Add yololib library

---
+++ iReSign/iReSign/yololib/yololib.h
@@ -0,0 +1,11 @@
+//
+//  yololib.h
+//  iReSign
+//
+//  Created by Joey on 16/3/23.
+//  Copyright © 2016年 nil. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+void injectDylibToBinary(NSString *binary, NSString *dylib);
